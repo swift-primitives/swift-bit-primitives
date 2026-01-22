efafd423512b69d935bae0508f2772d710146907
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-01-22 18:02:47 +0100

    Add Hash.Protocol conformance for Bit
    Add Bit+Hash.Protocol.swift with conformance extension. This follows
    [SEM-DEP-006] Domain Ordering: Bit (larger, specialized domain)
    depends on Hash.Protocol (smaller, fundamental domain).
    
    Also adds swift-hash-primitives as dependency.

--- Package.swift
+++ Package.swift
@@ -19,16 +19,18 @@ let package = Package(
     ],
     dependencies: [
         .package(path: "../swift-algebra-primitives"),
+        .package(path: "../swift-hash-primitives"),
         .package(path: "../swift-identity-primitives"),
-        .package(path: "../swift-index-primitives")
+        .package(path: "../swift-index-primitives"),
     ],
     targets: [
         .target(
             name: "Bit Primitives",
             dependencies: [
                 .product(name: "Algebra Primitives", package: "swift-algebra-primitives"),
+                .product(name: "Hash Primitives", package: "swift-hash-primitives"),
                 .product(name: "Identity Primitives", package: "swift-identity-primitives"),
-                .product(name: "Index Primitives", package: "swift-index-primitives")
+                .product(name: "Index Primitives", package: "swift-index-primitives"),
             ]
         ),
         .testTarget(
